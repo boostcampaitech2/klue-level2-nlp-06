afc2db1c1999f1e2e8058cdb589c42729d1a256d
MinJae <smj7172@naver.com>
2021-09-27 07:36:56 +0000

    Add macro_f1

--- code/train.py
+++ code/train.py
@@ -27,6 +27,9 @@ def klue_re_micro_f1(preds, labels):
     label_indices.remove(no_relation_label_idx)
     return sklearn.metrics.f1_score(labels, preds, average="micro", labels=label_indices) * 100.0
 
+def macro_f1(pred, labels):
+  return 0 
+
 def klue_re_auprc(probs, labels):
     """KLUE-RE AUPRC (with no_relation)"""
     labels = np.eye(30)[labels]
